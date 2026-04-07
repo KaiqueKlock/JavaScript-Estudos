10d6dc4ea477eb4a1b9d62503608ae7e37a5ef44
= <kaiqueklock@gmail.com>
2026-04-07 16:24:42 -0300

    Aula 04

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log('Hello My New World! Seems like I already have done this before, but lets go ! We will make this shit Happens!');
+console.log('Não vale aceitar mais sugestão de código kkkkkkkk');
+
