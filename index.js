fece33635cffd6d4177564cadd7d3ab619937ecd
= <kaiqueklock@gmail.com>
2026-04-13 07:57:42 -0300

    Aula 09

--- index.js
+++
@@ -1,3 +0,0 @@
-console.log('Hello My New World! Seems like I already have done this before, but lets go ! We will make this shit Happens!');
-console.log('Não vale aceitar mais sugestão de código kkkkkkkk');
-
