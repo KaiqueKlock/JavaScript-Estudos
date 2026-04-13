fcb0e7d6895972d257fbd3848e2b86e6160b72e6
= <kaiqueklock@gmail.com>
2026-04-13 15:08:56 -0300

    Aulas 11 - Exercicios 010 e 011

---
+++ Aula11/ex008.js
@@ -0,0 +1,8 @@
+var velocidade = 50
+console.log(`A velocidade do teu carro é ${velocidade} km/h`)
+if (velocidade > 60){
+    console.log(`Você ultrapassou a velocidade permitida! Multado!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança`)
+
