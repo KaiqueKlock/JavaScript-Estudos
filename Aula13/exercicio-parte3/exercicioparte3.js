4bc6aa9197b2292fd5250a9d822996c774a37116
= <kaiqueklock@gmail.com>
2026-05-08 17:57:35 -0300

    Exercicio parte 03 - solução02 com tratamento de erros de input

--- Aula13/exercicio-parte3/exercicioparte3.js
+++ Aula13/exercicio-parte3/exercicioparte3.js
@@ -1,12 +1,19 @@
 function verificarIdade(){
+    var data = new Date()
+    var ano = data.getFullYear()
     const form = document.getElementById("inputForm")
-    var idade = form.idade.value
+    var nascimento = form.idade.value
     var sexo = form.genero.value
     var button = window.document.getElementById("btnEnviar")
     var img = window.document.getElementById("imagem")
     var msg = window.document.getElementById("msg")
     let genero = "";
 
+    if(nascimento.length == 0 || nascimento > ano || nascimento == 0){
+        window.alert("ERRO[00X00] : Digite as informções corretamente")
+    } else{
+
+    let idade = ano - nascimento     
 
     if(idade <= 10){
         genero = (sexo === "Masculino") ? "você é um Menino" : " você é uma Menina";
@@ -36,4 +43,5 @@ function verificarIdade(){
     msg.innerHTML = `Detectamos que ${genero} e tem ${idade} anos de idade`
 
     }
-    
+
+}
